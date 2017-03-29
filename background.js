3f07d9e071e5f82aa939c8f835b3b3536b0cfe2b
roboticsound <roboticsound@tutanota.de>
2017-03-29 18:08:56 +0100

    rewrite background.js to work with more browsers

--- background.js
+++ background.js
@@ -1,35 +1,13 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-
-// When the extension is installed or upgraded ...
-chrome.runtime.onInstalled.addListener(function () {
-    // Replace all rules ...
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        // With a new rule ...
-        chrome.declarativeContent.onPageChanged.addRules([
-            {
-              // That fires when a page's URL contains...
-              conditions: [
-                new chrome.declarativeContent.PageStateMatcher({
-                    pageUrl: {
-                        //urlContains: "//www.imdb.com/",
-                        urlMatches: '\/\/www\.imdb.com\/.+\/tt\\d{7}\/'
-                    },
-                })
-              ],
-              // And shows the extension's page action.
-              actions: [new chrome.declarativeContent.ShowPageAction()]
-            }
-        ]);
-    });
-});
-
-browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (tab.url.match(/\/\/www\.imdb.com\/.+\/tt\d{7}\//)) {
-        chrome.pageAction.show(tabId);
+function handlePageAction(tabInfo) {
+  chrome.tabs.query({active:true,currentWindow:true},function(tabArray){
+    if (tabArray[0].url.match(/\/\/www\.imdb.com\/.+\/tt\d{7}\//)) {
+        chrome.pageAction.show(tabInfo.tabId);
     } else {
-        chrome.pageAction.hide(tabId);
+        chrome.pageAction.hide(tabInfo.tabId);
     }
-});
+  });
+};
+
+chrome.tabs.onActivated.addListener(handlePageAction);
+
+chrome.tabs.onUpdated.addListener(handlePageAction);
