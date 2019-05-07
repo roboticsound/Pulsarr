70c98f887e0c04e38cc5418a1327108769308d94
mithandir50 <mithandir50@gmail.com>
2019-05-07 17:20:24 +0200

    Adding senscritique.com support

--- background.js
+++ background.js
@@ -2,8 +2,8 @@
 
 function handlePageAction(tabInfo) {
   chrome.tabs.query({active:true,currentWindow:true},function(tabArray){
-    if (tabArray[0].url.match(/\/\/www\.imdb.com\/.+\/tt\d{7}\//) || tabArray[0].url.match(/.*thetvdb.com\/.*id\=\d{1,7}/) || tabArray[0].url.match(/.*trakt.tv\/(shows|movies)\//) || tabArray[0].url.match(/.*rottentomatoes.com\/(tv|m)\//) || tabArray[0].url.match(/.*themoviedb.org\/(tv|movie)\//) ) {
-        chrome.pageAction.show(tabInfo.tabId);
+    if (tabArray[0].url.match(/\/\/www\.imdb.com\/.+\/tt\d{7}\//) || tabArray[0].url.match(/\/\/www\.senscritique.com/) || tabArray[0].url.match(/.*thetvdb.com\/.*id\=\d{1,7}/) || tabArray[0].url.match(/.*trakt.tv\/(shows|movies)\//) || tabArray[0].url.match(/.*rottentomatoes.com\/(tv|m)\//) || tabArray[0].url.match(/.*themoviedb.org\/(tv|movie)\//) ) {
+      chrome.pageAction.show(tabInfo.tabId);
     } else {
         chrome.pageAction.hide(tabInfo.tabId);
     }
